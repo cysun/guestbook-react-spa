285e20f72d02b309e0a38eccbd61ede0b6ff55cd
Chengyu Sun <csun@calstatela.edu>
2019-12-03 22:02:45 +0000

    Load entries from backend API

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # GuestBook SPA in React
 
 In development mode, the backend API is assumed to be at http://localhost:8080.
-The `proxy` setting can be changed in package.jon.
+The `proxy` setting can be changed in package.jon. During remote debugging, if
+you see an error "Invalid Host Header", create a .env.local file and put in:
+
+`DANGEROUSLY_DISABLE_HOST_CHECK=true`
+
+This sets Webpack Dev Server's `disableHostCheck` option to true
+(see node_modules/react-scripts/config/webpackDevServer.config.js).
--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 
+import axios from "axios";
+
 function List() {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api").then(res => setEntries(res.data));
+  }, []);
+
   return (
     <>
       <Breadcrumb className="my-2">
         <Breadcrumb.Item active>Home</Breadcrumb.Item>
       </Breadcrumb>
-      <p>List of entries</p>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Message</th>
+            <th>Operations</th>
+          </tr>
+        </thead>
+        <tbody>
+          {entries.map(entry => (
+            <tr key={entry.id}>
+              <td>{entry.name}</td>
+              <td>{entry.message}</td>
+              <td>
+                <Link className="btn btn-primary mr-2" to={`/edit/${entry.id}`}>
+                  Edit
+                </Link>
+                <Button className="btn btn-danger">Delete</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <Link className="btn btn-primary" to="/add">
         Add Comments
       </Link>
